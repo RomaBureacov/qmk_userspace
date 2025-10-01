d2fa860352466b1ed68f9de68df41fce6be3e5d3
RomaBureacov <RomanBureacov@gmail.com>
2025-10-01 10:20:11 -0700

    appended missing keycodes for qwerty/engram default layer change

--- keyboards/splitkb/elora/keymaps/customQMK/keymap.c
+++ keyboards/splitkb/elora/keymaps/customQMK/keymap.c
@@ -431,7 +431,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------'      `------'                `---------------------------'      '------'
  */
     [_FUNCTIONS] = LAYOUT_myr(
-      _______, _______, _______, _______, _______, _______,             _______   , _______,          KC_INS , KC_F10 , KC_F11 , KC_F12  , TD(TD_VD_VU), KC_MPLY,
+      _______, QWERTY , ENGRAM , _______, _______, _______,             _______   , _______,          KC_INS , KC_F10 , KC_F11 , KC_F12  , TD(TD_VD_VU), KC_MPLY,
       _______, _______, _______, _______, _______, _______,             _______   , _______,          KC_SCRL, KC_F5  , KC_F6  , KC_F7   , KC_F8  , KC_F9  ,
       _______, _______, _______, _______, _______, _______,             _______   , _______,          KC_NUM , MU_TOGG, KC_F1  , KC_F2   , KC_F3  , KC_F4 ,
       _______, _______, _______, _______, _______, _______, _______,    TG_TD     , KC_TRNS, KC_PSCR, KC_CAPS, MU_NEXT, CK_TOGG, CK_RST  , CK_DOWN, CK_UP ,
